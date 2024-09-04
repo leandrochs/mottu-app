fc3221bf4bb2a1cc45fe3998c69afca2d5160c0c
leandrochs <leandrochagask@gmail.com>
2024-09-04 18:21:30 -0300

    migrations update eficiencia mecanico

---
+++ backend/MottuMaintenance/Migrations/20240904202518_EfficientMechanic.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MottuMaintenance.Migrations
+{
+    /// <inheritdoc />
+    public partial class EfficientMechanic : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ConsertoMotos_MecanicoId",
+                table: "ConsertoMotos",
+                column: "MecanicoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ConsertoMotos_Mecanicos_MecanicoId",
+                table: "ConsertoMotos",
+                column: "MecanicoId",
+                principalTable: "Mecanicos",
+                principalColumn: "MecanicoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ConsertoMotos_Mecanicos_MecanicoId",
+                table: "ConsertoMotos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ConsertoMotos_MecanicoId",
+                table: "ConsertoMotos");
+        }
+    }
+}
