92876611fee6500b4f1530fb652d99803be1dc5b
leandrochs <leandrochagask@gmail.com>
2024-09-04 12:41:23 -0300

    feat: Criar o componente mecanico-list

---
+++ frontend/src/app/mecanicos/mecanico-list/mecanico-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mecanico-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './mecanico-list.component.html',
+  styleUrl: './mecanico-list.component.scss'
+})
+export class MecanicoListComponent {
+
+}
